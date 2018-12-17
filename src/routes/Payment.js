cfc141c145b6e6de2e870f9c0cac64296c1c4439
Bruno Krebs <bruno.krebs@auth0.com>
2018-12-17 19:03:16 -0200

    adding gh-pages dev dependency and changing payment rules

--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -18,27 +18,15 @@ function Payment(props) {
       cancel={props.stepBack}
       cancelLabel="Go Back"
     >
-      <p>Articles submitted to the Guest Author program are categorized and payed through the following rules:</p>
-      <ArticleCategory>Theoretical Articles</ArticleCategory>
-      <p>Auth0 pays the following amounts to articles that explain concepts from a theoretical point of view:</p>
+      <p>The payment made for articles submitted to the Guest Author program are decided according to the following rules:</p>
       <ul>
-        <li>Up to 1000 words: 75 USD</li>
-        <li>Up to 1250 words: 100 USD</li>
-        <li>Up to 1500 words: 125 USD</li>
-        <li>More than 1500 words: 150 USD</li>
+        <li>Up to 1250 words: 75 USD</li>
+        <li>Between 1251 and 2500 words: 150 USD</li>
+        <li>More than 2500 words: 225 USD</li>
       </ul>
       <p>
-        Plus 50 USD if authors explain, with at least 200 words, how Auth0 can help on the context being studied.
-      </p>
-      <ArticleCategory>Technical Articles</ArticleCategory>
-      <p>For technical articles (those that include hands-on exercises), Auth0 pays the following amounts:</p>
-      <ul>
-        <li>Up to 1000 words: 125 USD</li>
-        <li>Up to 1500 words: 175 USD</li>
-        <li>More than 1500 words: 225 USD</li>
-      </ul>
-      <p>
-        Plus 75 USD if authors explain how to integrate Auth0 on their samples.
+        Plus 75 USD if authors explain, with at least 200 words, how Auth0 can help on the context being studied
+        (or on the sample being developed).
       </p>
     </Presentation>
   );
