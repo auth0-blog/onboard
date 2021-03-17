394eb7fe8b5b84c2f778fb6fe945abfb4b42fc2d
Diego Poza <diegopoza@gmail.com>
2021-03-17 16:28:20 -0300

    Update Payment.js

--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -18,15 +18,9 @@ function Payment(props) {
       cancel={props.stepBack}
       cancelLabel="Go Back"
     >
-      <p>The payment made for articles submitted to the Guest Author program are decided according to the following rules:</p>
-      <ul>
-        <li>Up to 1250 words: 75 USD</li>
-        <li>Between 1251 and 2500 words: 150 USD</li>
-        <li>More than 2500 words: 225 USD</li>
-      </ul>
+      <p>We pay $400 for each final article submitted to the Guest Author program.</p>
       <p>
-        Plus 75 USD if authors explain, with at least 200 words, how Auth0 can help on the context being studied
-        (or on the sample being developed).
+        Plus 50 USD if the blog draft is completed in 3 weeks or less.
       </p>
     </Presentation>
   );
